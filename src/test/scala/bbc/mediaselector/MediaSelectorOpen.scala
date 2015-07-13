98274949681656ce3b84c447a9e42210a8bcc369
aidylewis <adrian_lewis@hushmail.com>
2015-07-13 11:37:36 +0100

    new ms sim

--- src/test/scala/bbc/mediaselector/MediaSelectorOpen.scala
+++ src/test/scala/bbc/mediaselector/MediaSelectorOpen.scala
@@ -5,20 +5,33 @@ import scala.concurrent.duration._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class MediaSelectorOpen extends Simulation {
+class MediaSelector2379 extends Simulation {
 
   val httpProtocol = http
-    .header("X-IP-Address", "") // https://api.stage.bbc.co.uk/mediaselector/kipps?ip=194.159.80.39
+  // https://api.stage.bbc.co.uk/mediaselector/kipps?ip=194.159.80.39
+    .header("X-IP-Address", "")
 
   val open = csv("media-selector/open.csv").circular
+
+  val countStageServers = 4
+  val countProdServers = 16
+  val prodPeakRate = 1600
+  val prodFailureRate = 4000
+  val stagePeakRate = prodPeakRate * countStageServers / countProdServers
+  val stageFailureRate = prodFailureRate * countStageServers / countProdServers
 
   val scn = scenario("media-selector")
     .feed(open)
     .exec(http("open")
-    .get("http://open.stage.cwwtf.bbc.co.uk${openUrl}")
-    .check(status.is(200)))
+      .get("http://open.stage.cwwtf.bbc.co.uk${openUrl}")
+      .check(status.is(200)))
 
   setUp(scn.inject(
-      rampUsersPerSec(10) to(750) during(20 minutes) 
+      rampUsersPerSec(1) to(stagePeakRate)                  during(10 minutes) randomized,
+      constantUsersPerSec(stagePeakRate)                    during(20 minutes) randomized,
+      rampUsersPerSec(stagePeakRate) to(stageFailureRate)   during(10 minutes) randomized,
+      constantUsersPerSec(stageFailureRate)                 during(20 minutes) randomized,
+      rampUsersPerSec(stageFailureRate) to(stagePeakRate)   during(10 minutes) randomized,
+      constantUsersPerSec(stagePeakRate)                    during(10 minutes) randomized
   ).protocols(httpProtocol))
 }
